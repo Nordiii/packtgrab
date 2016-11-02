8b2dd63c7292fbb2e9347a0f01902c262788666a
Nordiii <niklas907@googlemail.com>
2016-11-02 11:28:11 +0100

    .

---
+++ src/main/java/Packtpub.java
@@ -0,0 +1,63 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
+/**
+ * Created by Nordiii on 02.11.2016.
+ */
+public class Packtpub {
+    private final WebClient webClient;
+
+    private final String defaultPage = "https://www.packtpub.com";
+    private final String loginFormID = "packt-user-login-form";
+    private final String freeBooksPage = "https://www.packtpub.com/packt/offers/free-learning";
+    private final String afterClaim = "https://www.packtpub.com/account/my-ebooks";
+
+    private final String email;
+    private final String password;
+
+
+    public Packtpub(String email, String password) throws IOException {
+        this.email = email;
+        this.password = password;
+        System.out.println("Opening Webclient");
+        webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setUseInsecureSSL(true);
+
+
+    }
+
+    public boolean login()throws IOException {
+
+            System.out.println("Accessing packtpub.com");
+            HtmlPage login = webClient.getPage(defaultPage);
+
+            System.out.println("Logging in");
+            HtmlForm form = login.getHtmlElementById(loginFormID);
+
+            form.getInputByName("email").setValueAttribute(email);
+            form.getInputByName("password").setValueAttribute(password);
+           return form.getInputByName("op").click().getUrl().toString().equals(defaultPage+"/index");
+    }
+
+    public boolean getFreeBook() throws IOException
+    {
+        HtmlPage freePage = webClient.getPage(freeBooksPage);
+
+        System.out.println("Free book: "+freePage.querySelector(".dotd-title").getTextContent().replaceAll("\t","").replaceAll("\n",""));
+
+       return webClient.getPage(defaultPage+freePage.querySelector(".twelve-days-claim").getAttributes().getNamedItem("href").getNodeValue())
+                .getUrl().toString().equals(afterClaim);
+    }
+
+    public void closeWebClient()
+    {
+        webClient.close();
+    }
+
+
+}
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,96 +1,68 @@
-        import com.gargoylesoftware.htmlunit.BrowserVersion;
-        import com.gargoylesoftware.htmlunit.WebClient;
-        import com.gargoylesoftware.htmlunit.html.HtmlForm;
-        import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-        import java.io.*;
-        import java.util.Properties;
-        import java.util.Scanner;
+import java.io.*;
+import java.util.Properties;
+import java.util.Scanner;
 
-/**
- * Created by Nordiii on 01.11.2016.
- */
 public class Start {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Checking properties file");
-        if(!checkProperties())
-        {
+        if (!checkProperties()) {
             System.out.println("Creating new user.properties");
-            if(!createProperties())
+            if (!createProperties())
                 System.out.println("Failed to create File");
         }
 
-
+        Packtpub site = null;
         Properties prop = getProperties();
-        System.out.println("Opening Webclient");
-        try (final WebClient webClient = new WebClient(BrowserVersion.CHROME)) {
-            webClient.getOptions().setJavaScriptEnabled(false);
-            webClient.getOptions().setUseInsecureSSL(true);
+        try {
+            site = new Packtpub(prop.getProperty("email"), prop.getProperty("password"));
 
-            System.out.println("Accessing packtpub.com");
-            HtmlPage login = webClient.getPage("https://www.packtpub.com/#");
-
-            System.out.println("Logging in");
-            HtmlForm form = login.getHtmlElementById("packt-user-login-form");
-
-            form.getInputByName("email").setValueAttribute(prop.getProperty("email"));
-            form.getInputByName("password").setValueAttribute(prop.getProperty("password"));
-            form.getInputByName("op").click();
-
-            System.out.println("Logged in, accessing free books");
-            HtmlPage freePage = webClient.getPage("https://www.packtpub.com/packt/offers/free-learning");
-
-            System.out.println("Free book: "+freePage.querySelector(".dotd-title").getTextContent().replaceAll("\t","").replaceAll("\n",""));
-
-            webClient.getPage("https://www.packtpub.com/"+freePage.querySelector(".twelve-days-claim").getAttributes().getNamedItem("href").getNodeValue());
+            if (!site.login()) {
+                System.out.println("Wasn't able to login");
+                throw new Exception();
+            }
+            if (site.getFreeBook())
+                System.out.println("Successfully claimed");
 
         } catch (Exception e) {
-            e.printStackTrace();
+
+        } finally {
+            if (site != null)
+                site.closeWebClient();
         }
+
     }
 
 
-    private static boolean checkProperties()
-    {
+    private static boolean checkProperties() {
         return new File("user.properties").exists();
     }
 
-    private static boolean createProperties()
-    {
+    private static boolean createProperties() {
         Properties prop = new Properties();
         Scanner sc = new Scanner(System.in);
-        try(OutputStream propFile = new FileOutputStream("user.properties"))
-        {
+        try (OutputStream propFile = new FileOutputStream("user.properties")) {
             System.out.println("Enter email:");
-            prop.setProperty("email",sc.nextLine());
+            prop.setProperty("email", sc.nextLine());
 
-            System.out.println("Enter email:");
-            prop.setProperty("password",sc.nextLine());
+            System.out.println("Enter password:");
+            prop.setProperty("password", sc.nextLine());
 
-            prop.store(propFile,null);
+            prop.store(propFile, null);
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             sc.close();
         }
         return checkProperties();
     }
 
-    private static Properties getProperties()
-    {
+    private static Properties getProperties() {
         Properties prop = new Properties();
-        try(InputStream propFile = new FileInputStream("user.properties"))
-        {
+        try (InputStream propFile = new FileInputStream("user.properties")) {
             prop.load(propFile);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return prop;
